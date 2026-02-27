1471b08fb3f9a7630a0384501eb3f7b639dc5b2a
rajh26 <moooookrt@gmail.com>
2026-02-27 19:48:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 int main() {
+    string password;
     string callerName;
-    cout << "--- FakeCall Pro System ---" << endl;
-    cout << "Enter the name of the caller: ";
-    getline(cin, callerName);
-    
-    cout << "\nSetting up the fake call..." << endl;
-    cout << "Incoming call from: [" << callerName << "]" << endl;
-    cout << "Status: ACTIVE" << endl;
 
+    cout << "--- FakeCall Pro Secure System ---" << endl;
+    cout << "Enter Access Code: ";
+    cin >> password;
+
+    if (password == "2026") { 
+        cout << "\n[Success] Access Granted!" << endl;
+        cout << "Enter Name of Caller: ";
+        cin >> callerName;
+        
+        cout << "Calling in 3 seconds..." << endl;
+        this_thread::sleep_for(chrono::seconds(3));
+
+        cout << "\n*************************" << endl;
+        cout << "   INCOMING CALL...      " << endl;
+        cout << "   " << callerName << "          " << endl;
+        cout << "*************************" << endl;
+    } else {
+        cout << "[Failed] Wrong Password!" << endl;
+    }
     return 0;
 }
